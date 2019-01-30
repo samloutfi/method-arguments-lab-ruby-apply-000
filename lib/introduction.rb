4b02e682d94312620f98d9bd6223090a25217945
Sam Loutfi <samloutfi@hotmail.com>
2019-01-30 23:12:16 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,6 +1,8 @@
-def introduction (name)
-  puts "Hi, my name is #{name}."
+def introduction_with_languages (name,language)
+  puts "Hi, my name is #{name} and I am leanring to program in #{language}."
 end
+
+
 
 
 
