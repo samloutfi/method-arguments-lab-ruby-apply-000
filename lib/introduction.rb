42dc7aa2d29a69bcdfcce708af92d774c0aa4066
Sam Loutfi <samloutfi@hotmail.com>
2019-01-30 23:13:29 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -2,7 +2,7 @@ def introduction_with_languages (name,language)
   puts "Hi, my name is #{name} and I am leanring to program in #{language}."
 end
 
-
+expect{introduction_with_languages("sam")}.to output("Hi, my name is Josh.n").to_stdout
 
 
 
